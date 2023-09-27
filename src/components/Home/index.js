134d1d2150d17f53ca9c558f0c979b26ac92e716
Steve Nathan de Sa <stevenathandesa@gmail.com>
2023-09-26 23:05:32 -0600

    delete gsap

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
-import Logo from './Logo';
 import Loader from 'react-loaders';
 
 const Home = () => {
@@ -33,9 +32,7 @@ const Home = () => {
                 </h1>
                 <h2>COMPUTER SCIENCE MAJOR @SDSMT</h2>
                 <Link to="/contact" className='flat-button'>REACH OUT TO ME!</Link>
-
             </div>
-            <Logo></Logo>
         </div>
         <Loader type="pacman" />
         </>
