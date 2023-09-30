f68956f93df6428fb9f5ab3dadf03d58a644a25f
Steve Nathan de Sa <stevenathandesa@gmail.com>
2023-09-29 23:55:39 -0600

    home icon revert

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -14,7 +14,7 @@ const Sidebar = () => (
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
-                <Home color="#4d4d4e" />
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
