74d7ab7f79408eec71868a71aca6027e15340505
Steve Nathan de Sa <stevenathandesa@gmail.com>
2024-01-21 21:27:58 -0700

    Logo Update

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-import {monkey, assistant, knight, doggies, vsss, sdmb, sdmw } from "./assets";
+import {monkey, assistant, knight, doggies, vsss, sdmb, sdmw, cat } from "./assets";
 
 
   
@@ -6,8 +6,8 @@ const experiences = [
 {
   title: "Software Engineering Intern",
   company_name: "Caterpillar MineStar Consortium, SDSMT", 
-  icon: sdmb, 
-  iconBg: "#001e4d",
+  icon: cat, 
+  iconBg: "#000000",
   date: "Nov 2023 - Present",
   point1: "Crafted and Deployed Dynamic Apps and Dashboards, leveraging API integration for Enhanced Functionality.",
   point2: "Collaborated with Executives worldwide, ensuring tailored Software Solutions aligned with Customer Requirements."
