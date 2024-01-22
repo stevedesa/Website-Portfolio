11b50bd21f1140e1b9863b3e4db5814e69a6ebbd
Steve Nathan de Sa <stevenathandesa@gmail.com>
2024-01-21 21:30:05 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@ const experiences = [
   title: "Software Engineering Intern",
   company_name: "Caterpillar MineStar Consortium, SDSMT", 
   icon: cat, 
-  iconBg: "#000000",
+  iconBg: "#1a1a1a",
   date: "Nov 2023 - Present",
   point1: "Crafted and Deployed Dynamic Apps and Dashboards, leveraging API integration for Enhanced Functionality.",
   point2: "Collaborated with Executives worldwide, ensuring tailored Software Solutions aligned with Customer Requirements."
